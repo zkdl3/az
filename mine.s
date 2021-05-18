f296c317066055eb083cbe5a1cbfb1e022af5251
deepmonero <70528104+deepmonero@users.noreply.github.com>
2021-05-19 03:06:46 +0530

    Create mine.s

---
+++ mine.s
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Change the following address to your ETH addr.
+ADDRESS=0xE3C91438804765bdCBF0fdb45337B82058a6a7Ee
+
+USERNAME=$ADDRESS.Father
+POOL=us1.ethermine.org:4444
+# Change SCHEME according to your POOL. For example:
+# ethash:     ethermine
+# ethproxy:   BTC.com, Ethermine, PandaMiner, Sparkpool
+# ethstratum: Antpool.com, BTC.com, F2pool, Huobipool.com, Miningpoolhub
+SCHEME=ethstratum
+
+./bminer -uri $SCHEME://$USERNAME@$POOL -api 127.0.0.1:1880 --max-temperature 75 -intensity 6
+
