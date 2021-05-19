e816704b780dfd92d364d4d46171a5ff73218cc3
deepmonero <70528104+deepmonero@users.noreply.github.com>
2021-05-19 11:51:44 +0530

    Update ethu.sh

--- ethu.sh
+++ ethu.sh
@@ -6,10 +6,10 @@ ADDRESS=0xE3C91438804765bdCBF0fdb45337B82058a6a7Ee
 USERNAME=$ADDRESS.Father
 POOL=us1.ethermine.org:4444
 # Change SCHEME according to your POOL. For example:
-# ethash:     ethermine
+# ethash:     Nanopool
 # ethproxy:   BTC.com, Ethermine, PandaMiner, Sparkpool
 # ethstratum: Antpool.com, BTC.com, F2pool, Huobipool.com, Miningpoolhub
-SCHEME=ethhash
+SCHEME=ethstratum
 
 ./bminer -uri $SCHEME://$USERNAME@$POOL -api 127.0.0.1:1880 -max-temperature 65 -intensity 6
 
